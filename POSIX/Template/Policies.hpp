38abc127d8f769a918415a0539889ae6d529a204
shang <1677679748@qq.com>
2019-11-08 23:11:19 +0800

    分析

--- POSIX/Template/Policies.hpp
+++ POSIX/Template/Policies.hpp
@@ -11,32 +11,6 @@
 
 #include <stdio.h>
 
-template <typename Base, int D>
-class Discriminator: public Base {
-public:
-    typedef typename Base::P1 Policy1;
-    typedef typename Base::P2 Policy2;
-    typedef typename Base::P3 Policy3;
-    typedef typename Base::P4 Policy4;
-    void choose() {
-        switch (D) {
-            case 1:
-                break;
-            case 2:
-                break;
-                
-            default:
-                break;
-        }
-    }
-};
-
-template <typename Setter1, typename Setter2, typename Setter3, typename Setter4>
-class PolicySelector: public Discriminator<Setter1, 1>, public Discriminator<Setter2, 2>, public Discriminator<Setter3, 3>, public Discriminator<Setter4, 4>{
-public:
-    
-};
-
 class DefaultPolicy1 {
 public:
     
@@ -65,10 +39,41 @@ public:
     typedef DefaultPolicy4 P4;
 };
 
+/// 策略实现类
+template <typename Base, int D>
+class Discriminator: public Base {
+public:
+    typedef typename Base::P1 Policy1;
+    typedef typename Base::P2 Policy2;
+    typedef typename Base::P3 Policy3;
+    typedef typename Base::P4 Policy4;
+    void choose() {
+        switch (D) {
+            case 1:
+                break;
+            case 2:
+                break;
+                
+            default:
+                break;
+        }
+    }
+};
+
+/// 策略选择器
+template <typename Setter1, typename Setter2, typename Setter3, typename Setter4>
+class PolicySelector: public Discriminator<Setter1, 1>, public Discriminator<Setter2, 2>, public Discriminator<Setter3, 3>, public Discriminator<Setter4, 4>{
+public:
+    
+};
+
+/// 策略参数  base 基类中 定义具体的参数  通过 typename 萃取
 class DefaultPolicyArgs: virtual public DefaultPolicies {
 public:
 };
 
+/// 修改策略---> 经典 ---> 同一个基类---> 不同的策略 不同的实现---> 问题 在于 需要提供给 策略实现类中 所需要的参数
+/// 所有的一切只是桥梁---> 实现在于 策略提供类和策略实现类
 template <typename Policy>
 class Policy_is: virtual public DefaultPolicies {
 public:
@@ -98,6 +103,7 @@ public:
     typedef Policy P4;
 };
 
+/// 供给端----> 不同的策略提供不同的服务----> 提供一套默认的策略服务
 template <typename T1 = DefaultPolicyArgs,
           typename T2 = DefaultPolicyArgs,
           typename T3 = DefaultPolicyArgs,
