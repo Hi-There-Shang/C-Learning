766578ca75349c04e3f0e804a38c7966c1d5cdf5
shang <1677679748@qq.com>
2019-11-16 23:41:56 +0800

    sleep

--- POSIX/Swift/JsonTraits.hpp
+++ POSIX/Swift/JsonTraits.hpp
@@ -11,7 +11,11 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <boost/regex.hpp>
+#include <boost/xpressive/basic_regex.hpp>
 #include <boost/container/string.hpp>
+
+typedef boost::container::string StringRef;
 
 template <typename T> struct ObjectTraits {};
 
@@ -48,7 +52,7 @@ template <class T>
 struct has_ScalarTraits {
     ///  std::ostream  ---> llvm::raw_ostream
     using Signature_output = void (*)(const T&, std::ostream &);
-    using Signature_mustQuote = bool (*)(std::string &);
+    using Signature_mustQuote = bool (*)(StringRef);
     
     template <class U>
     static char test(SameType<Signature_output, &U::output>*, SameType<Signature_mustQuote, &U::mustQuote>);
@@ -58,5 +62,102 @@ struct has_ScalarTraits {
 public:
     static bool const value = (test<ScalarTraits<T>>(nullptr, nullptr) == 1);
 };
+
+template <class T>
+struct has_ScalarReferenceTratis {
+    using Signature_StringRef = StringRef (*)(const T&);
+    using Signature_mustQuout = bool (*)(StringRef);
+    
+    template <class U>
+    static char test(SameType<Signature_StringRef, &U::stringRef>*, SameType<Signature_mustQuout, &U::mustQuote>*);
+    
+    template <class U>
+    static bool test(...);
+    
+public:
+    static bool const value = (test<ScalarReferenceTraits<T>>(nullptr, nullptr) == 1);
+};
+
+template <class T>
+struct has_ObjectTraits
+{
+    using Signature_mapping = void (*)(class Output &, T &);
+    
+    template <typename U>
+    static char test(SameType<Signature_mapping, &U::mapping>*);
+    
+    template <typename U>
+    static double test(...);
+    
+public:
+    static bool const value = (sizeof(test<ObjectTraits<T> >(nullptr)) == 1);
+};
+
+
+template <class T>
+struct has_ObjectValidateTraits
+{
+    using Signature_validate = StringRef (*)(class Output &, T &);
+    
+    template <typename U>
+    static char test(SameType<Signature_validate, &U::validate>*);
+    
+    template <typename U>
+    static double test(...);
+    
+public:
+    static bool const value = (sizeof(test<ObjectTraits<T> >(nullptr)) == 1);
+};
+
+template <class T>
+struct has_ArrayMethodTraits
+{
+    using Signature_size = size_t (*)(class Output &, T &);
+    
+    template <typename U>
+    static char test(SameType<Signature_size, &U::size>*);
+    
+    template <typename U>
+    static double test(...);
+    
+public:
+    static bool const value =  (sizeof(test<ArrayTraits<T> >(nullptr)) == 1);
+};
+
+template<typename T>
+struct has_ArrayTraits : public std::integral_constant<bool,
+has_ArrayMethodTraits<T>::value > { };
+
+// Test if NullableTraits<T> is defined on type T.
+template <class T>
+struct has_NullableTraits
+{
+    using Signature_isNull = bool (*)(T &);
+    
+    template <typename U>
+    static char test(SameType<Signature_isNull, &U::isNull> *);
+    
+    template <typename U>
+    static double test(...);
+    
+public:
+    static bool const value =
+    (sizeof(test<NullableTraits<T>>(nullptr)) == 1);
+};
+
+inline bool isNumber(StringRef S) {
+    /static const char DecChars[] = "0123456789";
+    
+    if (S.find_first_not_of(DecChars) == StringRef::npos)
+        return true;
+    using Regex = boost::xpressive::sregex;
+    Regex reg = Regex::compile("^(\\.[0-9]+|[0-9]+(\\.[0-9]*)?)([eE][-+]?[0-9]+)?$");
+//    boost::regex_match(<#const std::basic_string<charT, ST, SA> &s#>, <#const basic_regex<charT, traits> &e#>)
+//    boost::regex_match(S, reg);
+//    if (boost::regex_match(S, reg)) {
+//        return true;
+//    }
+    return false;
+}
 
 #endif /* JsonTraits_hpp */
